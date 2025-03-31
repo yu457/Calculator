4a0c75fa03c0d484d926a6d8b7fcde050ec85431
yu457 <113153008@gas.fgu.edu.tw>
2025-03-31 14:42:20 +0800

    完成四則運算按鍵、等於按鍵、小數點按鍵
    等於 - switch case 條件判斷

--- Form1.cs
+++ Form1.cs
@@ -28,12 +28,39 @@ namespace Calculator
 
         private void btnDot_Click(object sender, EventArgs e)
         {
-
+            // 確認輸入文字框中完全沒有小數點
+            if (txtNumber.Text.IndexOf(".") == -1)
+                txtNumber.Text = txtNumber.Text + ".";
         }
 
         private void btnEqual_Click(object sender, EventArgs e)
         {
+            float finalResults = 0f; //宣告最後計算結果變數
+            secondNumber = Convert.ToSingle(txtNumber.Text); //將輸入文字框轉換成浮點數，存入第二個數字的全域變數
 
+            //依照四則運算符號的選擇，進行加減乘除
+            switch (operators)
+            {
+                case 0:
+                    finalResults = firstNumber + secondNumber;
+                    break;
+                case 1:
+                    finalResults = firstNumber - secondNumber;
+                    break;
+                case 2:
+                    finalResults = firstNumber * secondNumber;
+                    break;
+                case 3:
+                    finalResults = firstNumber / secondNumber;
+                    break;
+            }
+
+            txtNumber.Text = string.Format("{0:0.##########}", finalResults); //在輸入文字框中，顯示最後計算結果，並且轉換成格式化的字串內容
+
+            //重置所有全域變數
+            firstNumber = 0f;
+            secondNumber = 0f;
+            operators = -1;
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
@@ -54,14 +81,14 @@ namespace Calculator
         {
             firstNumber = Convert.ToSingle(txtNumber.Text); //將輸入文字框轉換成浮點數，存入第一個數字的全域變數
             txtNumber.Text = "0"; //重新將輸入文字框重新設定為0
-            operators = 2; //選擇「加」號
+            operators = 2; //選擇「乘」號
         }
 
         private void btnDivide_Click(object sender, EventArgs e)
         {
             firstNumber = Convert.ToSingle(txtNumber.Text); //將輸入文字框轉換成浮點數，存入第一個數字的全域變數
             txtNumber.Text = "0"; //重新將輸入文字框重新設定為0
-            operators = 3; //選擇「加」號
+            operators = 3; //選擇「除」號
         }
 
         private void btnZero_Click(object sender, EventArgs e)
