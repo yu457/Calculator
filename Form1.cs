cf5163552897213cf950b986d16cd10313018b4b
yu457 <113153008@gas.fgu.edu.tw>
2025-06-12 10:33:33 +0800

    新增百分比按鍵之程式

--- Form1.cs
+++ Form1.cs
@@ -11,7 +11,7 @@ using System.Xml;
 
 namespace Calculator
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
         public Form1()
         {
@@ -21,7 +21,7 @@ namespace Calculator
         // 全域變數
         float firstNumber, secondNumber; // firstNumber 儲存第一個數字，secondNumber 儲存第二個數字
         int operators = -1; // 記錄選擇哪一種運算符號？0:加、1:減、2:乘、3:除、-1:重新設定
-       
+
         private void btnClear_Click(object sender, EventArgs e)
         {
             txtNumber.Text = "0";
@@ -35,7 +35,7 @@ namespace Calculator
 
             // 確認輸入文字框中完全沒有小數點
             if (txtNumber.Text.IndexOf(".") == -1)
-                txtNumber.Text = txtNumber.Text + ".";  
+                txtNumber.Text = txtNumber.Text + ".";
         }
 
         private void btnEqual_Click(object sender, EventArgs e)
@@ -114,7 +114,7 @@ namespace Calculator
         {
             Add_Number("1");
             //if (txtNumber.Text == "0")
-                //txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
+            //txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
             //txtNumber.Text += "1";
             // 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的        }
         }
@@ -153,7 +153,7 @@ namespace Calculator
         {
             Add_Number("8");
         }
-+        private void btnNine_Click(object sender, EventArgs e)
+        private void btnNine_Click(object sender, EventArgs e)
         {
             Add_Number("9");
         }
@@ -176,10 +176,18 @@ namespace Calculator
 
         private void btnPercent_Click(object sender, EventArgs e)
         {
+            // 將目前顯示的文字轉成 float
             float number = Convert.ToSingle(txtNumber.Text);
-            number = number / 100f;
-            txtNumber.Text = string.Format("{0:P2}", number); // 顯示成百分比格式
+
+            // 除以 100，取得百分比值
+            float percentValue = number / 100f;
+
+            // 顯示計算後的數值（不加 %，避免後續運算錯誤）
+            txtNumber.Text = percentValue.ToString("0.##########");
         }
+
     }
+
+
 
 }
